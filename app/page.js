cff40e6fc75de237f5216d31baab1dd030b514ab
mohdsameerahmed346-lgtm <mohdsameerahmed346@gmail.com>
2026-04-06 00:33:01 +0530

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>HOME PAGE</h1>
+
+      <a href="/dashboard">
+        <button>Go Dashboard</button>
+      </a>
+    </main>
+  );
+}
