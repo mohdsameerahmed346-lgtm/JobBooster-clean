8657fd7cc58a27757e2e3fb92f5600eea99454aa
mohdsameerahmed346-lgtm <mohdsameerahmed346@gmail.com>
2026-04-06 07:25:52 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,11 +1,16 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>HOME PAGE</h1>
+"use client";
 
-      <a href="/dashboard">
-        <button>Go Dashboard</button>
-      </a>
+import { useRouter } from "next/navigation";
+
+export default function Home() {
+  const router = useRouter();
+
+  return (
+    <main style={{ padding: 20 }}>
+      <h1>🚀 JobBoost 2.0</h1>
+      <button onClick={() => router.push("/login")}>
+        Go to Login
+      </button>
     </main>
   );
 }
