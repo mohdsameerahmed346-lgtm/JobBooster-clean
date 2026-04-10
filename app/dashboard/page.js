3a22642bb5b029c857b16e81bafd8a6a57b1ec32
mohdsameerahmed346-lgtm <mohdsameerahmed346@gmail.com>
2026-04-10 06:40:19 +0530

    Update page.js

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,11 +1,12 @@
-""use client";
+"use client";
 
 export default function Test() {
   return (
-    <div>
-      <button onClick={() => alert("Working")}>
-        Test Button
+    <div style={{ background: "red", height: "100vh" }}>
+      <h1>TEST PAGE WORKING</h1>
+      <button onClick={() => alert("CLICK WORKS")}>
+        CLICK ME
       </button>
     </div>
   );
-    }
+  }
