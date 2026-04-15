394e84e55bebac1f415e4582982229b6593de4fe
mohdsameerahmed346-lgtm <mohdsameerahmed346@gmail.com>
2026-04-15 15:55:31 +0530

    Create page.js

---
+++ app/(dashboard)/pricing/page.js
@@ -0,0 +1,32 @@
+"use client";
+
+export default function Pricing() {
+  const upgrade = () => {
+    localStorage.setItem("premium", "true");
+    alert("Premium Activated 💎");
+  };
+
+  return (
+    <div>
+      <h1 className="text-3xl mb-6">💎 Pricing</h1>
+
+      <div className="grid md:grid-cols-2 gap-6">
+
+        <div className="bg-gray-900 p-6 rounded">
+          <h2>Free</h2>
+          <p>₹0</p>
+        </div>
+
+        <div className="bg-blue-600 p-6 rounded">
+          <h2>Premium</h2>
+          <p>₹199/month</p>
+
+          <button onClick={upgrade} className="mt-4 bg-black px-5 py-2 rounded">
+            Upgrade
+          </button>
+        </div>
+
+      </div>
+    </div>
+  );
+    }
