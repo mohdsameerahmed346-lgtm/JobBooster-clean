61c00b57bf8a48149d054e87088c16f3b79017c6
mohdsameerahmed346-lgtm <mohdsameerahmed346@gmail.com>
2026-04-15 15:53:23 +0530

    Create page.js

---
+++ app/(dashboard)/account/page.js
@@ -0,0 +1,37 @@
+"use client";
+
+export default function Account() {
+  const upgrade = () => {
+    localStorage.setItem("premium", "true");
+    alert("Premium Activated 💎");
+  };
+
+  const logout = () => {
+    alert("Logged out");
+  };
+
+  const clear = () => {
+    localStorage.removeItem("history");
+    alert("History cleared");
+  };
+
+  return (
+    <div>
+      <h1 className="text-3xl mb-6">⚙️ Account</h1>
+
+      <div className="space-y-4">
+        <button onClick={upgrade} className="bg-blue-600 px-5 py-2 rounded">
+          Upgrade
+        </button>
+
+        <button onClick={clear} className="bg-red-600 px-5 py-2 rounded">
+          Delete History
+        </button>
+
+        <button onClick={logout} className="bg-gray-700 px-5 py-2 rounded">
+          Logout
+        </button>
+      </div>
+    </div>
+  );
+    }
