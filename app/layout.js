108184ed426780df4f0c5c7b08d85b33c64d8ab3
mohdsameerahmed346-lgtm <mohdsameerahmed346@gmail.com>
2026-04-15 05:48:29 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,11 @@
 import "./globals.css";
 
-export const metadata = {
-  title: "JobBooster",
-  description: "AI Job Preparation SaaS",
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-gray-950 text-white">
+      <body className="bg-slate-950 text-white">
         {children}
       </body>
     </html>
   );
-}
+    }
