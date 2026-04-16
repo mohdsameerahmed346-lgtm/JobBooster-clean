0688f4bd1e6bf8b436e015cbb3d1598018fc83da
mohdsameerahmed346-lgtm <mohdsameerahmed346@gmail.com>
2026-04-16 07:02:07 +0530

    Update route.js

--- app/api/history/route.js
+++ app/api/history/route.js
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import { NextResponse } from "next/server";
 import { getFirestore } from "firebase-admin/firestore";
 import { initializeApp, getApps, cert } from "firebase-admin/app";
