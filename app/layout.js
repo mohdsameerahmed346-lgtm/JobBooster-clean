9ad54c61124c19682d5cf95ca8be03c99997505a
mohdsameerahmed346-lgtm <mohdsameerahmed346@gmail.com>
2026-04-16 05:53:53 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -3,9 +3,7 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-slate-950 text-white">
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
-    }
+}
