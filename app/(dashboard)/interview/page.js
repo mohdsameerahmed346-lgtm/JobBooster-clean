ed0c9df1ec8536e7d43e058c8abfa80616aa54fd
mohdsameerahmed346-lgtm <mohdsameerahmed346@gmail.com>
2026-04-18 06:02:35 +0530

    Update page.js

--- app/(dashboard)/interview/page.js
+++ app/(dashboard)/interview/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import Skeleton from "@/components/Skeleton";
+import Skeleton from "../../../components/Skeleton";
 
 export default function InterviewPage() {
   const [role, setRole] = useState("");
