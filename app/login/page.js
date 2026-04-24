51ea42b43f0cac75b8d8f6bd93663afcc3564267
mohdsameerahmed346-lgtm <mohdsameerahmed346@gmail.com>
2026-04-24 06:43:05 +0530

    Change login method to use Google sign-in

--- app/login/page.js
+++ app/login/page.js
@@ -1,51 +1,26 @@
 "use client";
 
-import { useState } from "react";
-import { auth } from "../../lib/firebase";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { signInWithPopup } from "firebase/auth";
+import { auth, provider } from "../../lib/firebase";
 import { useRouter } from "next/navigation";
 
 export default function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const router = useRouter();
 
   const login = async () => {
     try {
-      await signInWithEmailAndPassword(auth, email.trim(), password.trim());
-      router.push("/dashboard");
+      await signInWithPopup(auth, provider);
+      router.push("/skill-gap");
     } catch (err) {
-      alert(err.code);
+      alert("Login failed");
     }
   };
 
   return (
-    <div className="h-screen flex items-center justify-center bg-slate-950 text-white">
-
-      <div className="bg-gray-900 p-6 rounded-xl w-80 space-y-4">
-        <h1 className="text-xl font-bold">Login</h1>
-
-        <input
-          placeholder="Email"
-          onChange={(e) => setEmail(e.target.value)}
-          className="w-full p-2 bg-black rounded"
-        />
-
-        <input
-          type="password"
-          placeholder="Password"
-          onChange={(e) => setPassword(e.target.value)}
-          className="w-full p-2 bg-black rounded"
-        />
-
-        <button
-          onClick={login}
-          className="bg-blue-600 w-full py-2 rounded"
-        >
-          Login
-        </button>
-      </div>
-
+    <div className="h-screen flex items-center justify-center">
+      <button onClick={login} className="btn-primary">
+        Login with Google
+      </button>
     </div>
   );
-    }
+                       }
