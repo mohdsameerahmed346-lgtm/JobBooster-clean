92cb0bea9347ce2543d98a686e92b43d763f629b
mohdsameerahmed346-lgtm <mohdsameerahmed346@gmail.com>
2026-04-24 20:51:32 +0530

    Refactor Skeleton component structure and styling

--- components/Skeleton.js
+++ components/Skeleton.js
@@ -1,7 +1,17 @@
-export default function Skeleton({ className }) {
+"use client";
+
+export default function Skeleton() {
   return (
-    <div
-      className={`bg-gray-800 animate-pulse rounded ${className}`}
-    />
+    <div className="animate-pulse space-y-4">
+
+      <div className="grid grid-cols-2 gap-4">
+        <div className="h-24 bg-gray-800 rounded-xl" />
+        <div className="h-24 bg-gray-800 rounded-xl" />
+      </div>
+
+      <div className="h-20 bg-gray-800 rounded-xl" />
+      <div className="h-32 bg-gray-800 rounded-xl" />
+
+    </div>
   );
-}
+    }
